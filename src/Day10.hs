4f8c9bde3eade3afec6dbea17604d62949835c5a
David Landry <da.landry@proton.me>
2023-12-27 00:06:01 +0100

    Day 10 part 2

--- src/Day10.hs
+++ src/Day10.hs
@@ -16,60 +16,70 @@ data Point
   deriving (Eq, Show)
 
 part1 :: IO Int
-part1 = do
-  matrix <- parseMatrix
-
-  let startingIndex = M.findIndex Start matrix
-  let sizeOfLoop = loopSize 0 startingIndex startingIndex matrix
-
-  return $ sizeOfLoop `div` 2
-
-loopSize :: Int -> (Int, Int) -> (Int, Int) -> M.Matrix Point -> Int
-loopSize size previousIdx (m, n) matrix
-  | isBeginningFromStart = loopSize (size + 1) (m, n) firstConnectingPipeFromStart matrix
-  | hasLoopedBackToStart = size
-  | otherwise = loopSize (size + 1) (m, n) nextPipe matrix
-  where
-    point = matrix A.! (m, n)
-    hasLoopedBackToStart = point == Start && previousIdx /= (m, n)
-    isBeginningFromStart = point == Start && previousIdx == (m, n)
-
-    upIdx = (m - 1, n)
-    rightIdx = (m, n + 1)
-    downIdx = (m + 1, n)
-    leftIdx = (m, n - 1)
-
-    up = matrix A.! upIdx
-    right = matrix A.! rightIdx
-    down = matrix A.! downIdx
-
-    firstConnectingPipeFromStart
-      | previousIdx /= upIdx && up `elem` [Vertical, SouthEast, SouthWest] = upIdx
-      | previousIdx /= rightIdx && right `elem` [Horizontal, NorthWest, SouthWest] = rightIdx
-      | previousIdx /= downIdx && down `elem` [Vertical, NorthEast, NorthWest] = downIdx
-      | otherwise = leftIdx
-
-    nextPipe
-      | goUp = upIdx
-      | goRight = rightIdx
-      | goDown = downIdx
-      | otherwise = leftIdx
-      where
-        goUp =
-          previousIdx == downIdx && point == Vertical
-            || previousIdx == leftIdx && point == NorthWest
-            || previousIdx == rightIdx && point == NorthEast
-        goRight =
-          previousIdx == leftIdx && point == Horizontal
-            || previousIdx == upIdx && point == NorthEast
-            || previousIdx == downIdx && point == SouthEast
-        goDown =
-          previousIdx == upIdx && point == Vertical
-            || previousIdx == leftIdx && point == SouthWest
-            || previousIdx == rightIdx && point == SouthEast
+part1 = (`div` 2) . length . getLoop <$> parseMatrix
 
 part2 :: IO Int
-part2 = return 20
+part2 = countEnclosed . getLoop <$> parseMatrix
+
+getLoop :: M.Matrix Point -> [(Int, Int)]
+getLoop matrix = go startIndex startIndex
+  where
+    startIndex = M.findIndex Start matrix
+
+    go prevIdx currIdx
+      | hasLoopedBackToStart = []
+      | isBeginningFromStart = currIdx : go currIdx firstConnectingPipeFromStart
+      | otherwise = currIdx : go currIdx nextPipeIdx
+      where
+        point = matrix A.! currIdx
+        hasLoopedBackToStart = point == Start && prevIdx /= currIdx
+        isBeginningFromStart = point == Start && prevIdx == currIdx
+
+        (m, n) = currIdx
+
+        upIdx = (m - 1, n)
+        rightIdx = (m, n + 1)
+        downIdx = (m + 1, n)
+        leftIdx = (m, n - 1)
+
+        up = matrix A.! upIdx
+        right = matrix A.! rightIdx
+        down = matrix A.! downIdx
+
+        firstConnectingPipeFromStart
+          | prevIdx /= upIdx && up `elem` [Vertical, SouthEast, SouthWest] = upIdx
+          | prevIdx /= rightIdx && right `elem` [Horizontal, NorthWest, SouthWest] = rightIdx
+          | prevIdx /= downIdx && down `elem` [Vertical, NorthEast, NorthWest] = downIdx
+          | otherwise = leftIdx
+
+        nextPipeIdx
+          | goUp = upIdx
+          | goRight = rightIdx
+          | goDown = downIdx
+          | otherwise = leftIdx
+          where
+            goUp =
+              prevIdx == downIdx && point == Vertical
+                || prevIdx == leftIdx && point == NorthWest
+                || prevIdx == rightIdx && point == NorthEast
+            goRight =
+              prevIdx == leftIdx && point == Horizontal
+                || prevIdx == upIdx && point == NorthEast
+                || prevIdx == downIdx && point == SouthEast
+            goDown =
+              prevIdx == upIdx && point == Vertical
+                || prevIdx == leftIdx && point == SouthWest
+                || prevIdx == rightIdx && point == SouthEast
+
+-- | Pick's theorem + shoelace formula
+countEnclosed :: [(Int, Int)] -> Int
+countEnclosed path = shoelace path - (length path `div` 2) + 1
+
+shoelace :: [(Int, Int)] -> Int
+shoelace path = (`div` 2) . abs . sum $ zipWith determinant path shiftedPath
+  where
+    determinant (x1, y1) (x2, y2) = (x1 * y2) - (y1 * x2)
+    shiftedPath = tail path ++ [head path]
 
 parseMatrix :: IO (M.Matrix Point)
 parseMatrix = M.fromStrings parseChar <$> readStrings "inputs/day10.txt"
